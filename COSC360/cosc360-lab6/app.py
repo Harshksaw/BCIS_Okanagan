d149ae378760edc7f2d06b08636f27c3a2dd8b83
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-10-24 23:40:43 -0700

    Add Flask application with Docker support, static HTML page, and ECS configuration

---
+++ COSC360/cosc360-lab6/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import secrets
+import os
+
+app = Flask(__name__)
+
+
+myID = secrets.token_hex(8)
+
+@app.route("/")
+def home():
+    return "Hello from COSC 360 Lab!"
+
+@app.route("/static-page")
+def static_page():
+    with open("static.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+@app.route("/get-request")
+def get_request():
+    name = request.args.get("name", "Stranger")
+    return f"Hello, {name}! 👋"
+
+@app.route("/myid")
+def show_myid():
+    return myID
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 80))
+    app.run(host="0.0.0.0", port=port)
